8af3bf300d3f70471e7b695587dc7c1035fb4da3
汤阳 <tangyang@tangyangdeMac-mini.local>
2017-11-15 16:45:47 +0800

    update

---
+++ index.ios.js
@@ -0,0 +1,61 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+export default class RNProject extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Hello world123!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+        <View style={styles.viewOne} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  },
+  viewOne: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'powderblue'
+  }
+})
+
+AppRegistry.registerComponent('RNProject', () => RNProject)
